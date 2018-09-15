56adbb4354e2fe745629599bd89a9d35fd6280fc
404Error <lanpingcode@163.com>
2018-09-15 17:44:48 +0800

    1

--- lan/demo/src/test/java/com/example/demo/Chapter6ApplicationTests.java
+++ lan/demo/src/test/java/com/example/demo/Chapter6ApplicationTests.java
@@ -1,4 +1,40 @@
 package com.example.demo;
 
+import com.example.demo.dto.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("dev")
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class Chapter6ApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void redisTest() {
+        // redis存储数据
+        String key = "name";
+        redisTemplate.opsForValue().set(key, "yukong");
+        // 获取数据
+        String value = (String) redisTemplate.opsForValue().get(key);
+        System.out.println("获取缓存中key为" + key + "的值为：" + value);
+
+        User user = new User();
+        user.setUsername("yukong");
+        user.setSex(18);
+        user.setId(1L);
+        String userKey = "yukong";
+        redisTemplate.opsForValue().set(userKey, user);
+        User newUser = (User) redisTemplate.opsForValue().get(userKey);
+        System.out.println("获取缓存中key为" + userKey + "的值为：" + newUser);
+
+    }
+
 }
