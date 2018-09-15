43a2c129dd96e06250bb3a07f2836c53d455c317
404Error <lanpingcode@163.com>
2018-09-15 17:43:48 +0800

    1

---
+++ lan/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.src;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+
+    @GetMapping("/test1")
+    public String test1(){
+
+        return "hello world";
+    }
+
+
+}
---
+++ lan/demo/src/test/java/com/example/demo/Chapter6ApplicationTests.java
@@ -0,0 +1,4 @@
+package com.example.demo;
+
+public class Chapter6ApplicationTests {
+}
